e4a86e66c52466c211d27ffaa24d57d8c5cc40fa
Aa' Icoll <tu.mts01sgs@gmail.com>
2026-08-21 11:18:43 +0700

    Update test-sheet.ts

--- test-sheet.ts
+++ test-sheet.ts
@@ -3,8 +3,9 @@ const { getNilaiSiswaDoc } = require('./src/lib/google-sheets');
 (async () => {
   const doc = await getNilaiSiswaDoc();
   const sheet = doc.sheetsByTitle['PK'];
-  await sheet.loadCells('A1:AC500');
-  sheet.getCell(2, 1).value = null;
-  await sheet.saveUpdatedCells();
+  await sheet.loadCells('A1:AC2');
+  let row = [];
+  for (let j = 0; j < 29; j++) row.push(sheet.getCell(0, j).value);
+  console.log('Row 1:', row);
 })();
 
